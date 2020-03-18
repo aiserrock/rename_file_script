6b217a215ec2cc04af373dd1ad594e8ee494925b
aiserrock <aicserrock@gmail.com>
2020-03-18 16:50:16 +0300

    fix bug

--- rename_file_script.py
+++ rename_file_script.py
@@ -4,20 +4,14 @@ import re
 ESCAPEWORD = 'escape'
 path = "/mnt/f/newfolder/"
 listOfFiles = os.listdir(path)
+countOfFiles = len(listOfFiles)
+os.chdir(path)
+for i in range(0,countOfFiles):
+	match = re.search(ESCAPEWORD,listOfFiles[i])
+	if not match:
+		os.rename(path+listOfFiles[i], path+'Saying'+" "+str(i+1)+'.txt')
 
-for list in listOfFiles:
-	match = re.search(ESCAPEWORD,list)
-	if match:
-		listOfFiles.remove(list)
-i=0 
-for list in listOfFiles:
-	os.rename(path+list, path+'Общение'+" "+str(i+1)+'.mp4')
-	i+=1
-# countOfFiles = len(listOfFiles)
-# os.chdir(path)
-# for i in range(0,countOfFiles):
-# 	os.rename(path+listOfFiles[i], 'Комплимент'+''+str(i+1)+'.mp4')
-
+#debugging information
 listOfFiles = os.listdir(path)
 for list in listOfFiles:
 	print(list)
